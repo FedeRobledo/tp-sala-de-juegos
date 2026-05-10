bd5872e7afcead50b8c817d271f7d816e7f36617
Federico <federico.m.robledo@gmail.com>
2026-05-10 14:00:04 -0300

    feat: estructura base y navegación inicial para el stprint 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
+import { QuienSoy } from './components/quien-soy/quien-soy';
+import { NotFound } from './components/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'quien-soy', component: QuienSoy },
+  { path: '**', component: NotFound },
+];
