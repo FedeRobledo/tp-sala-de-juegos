ae3b46da315c9f0d15a5e29aee37785c8d7e4ab1
Federico <federico.m.robledo@gmail.com>
2026-05-19 20:23:49 -0300

    add: refactor del servicio de github para mover logica desde el componente quien soy al servicio

--- src/app/services/github.ts
+++ src/app/services/github.ts
@@ -1,6 +1,5 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 export interface GithubUser {
   login: string;
@@ -21,7 +20,25 @@ export class Github {
   private http = inject(HttpClient);
   private readonly apiUrl = 'https://api.github.com/users/FedeRobledo';
 
-  getUser(): Observable<GithubUser> {
-    return this.http.get<GithubUser>(this.apiUrl);
+  user = signal<GithubUser | null>(null);
+  loading = signal(false);
+  error = signal(false);
+
+  loadUser(): void {
+    this.loading.set(true);
+    this.error.set(false);
+    this.user.set(null);
+
+    this.http.get<GithubUser>(this.apiUrl).subscribe({
+      next: (data) => {
+        this.user.set(data);
+        this.loading.set(false);
+      },
+      error: (err) => {
+        console.error('Error al consultar GitHub:', err);
+        this.error.set(true);
+        this.loading.set(false);
+      },
+    });
   }
 }
